7db1861597a2a474f57b3b95f209a5d9428a3b3c
Hasan <hasan.mahdi7227@gmail.com>
2026-07-29 02:00:48 +0300

    initialized the new mvn project and added all slugs from the companies' names to redis

---
+++ job-validator/src/test/java/com/distributed/job_validator/JobValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distributed.job_validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
