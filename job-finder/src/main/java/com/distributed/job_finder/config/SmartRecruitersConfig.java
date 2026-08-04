0b11802e77a9297ddbe711e01205bda25d504cc9
Hasan <hasan.mahdi7227@gmail.com>
2026-08-05 01:41:55 +0300

    added config file to fetch the baord tokens for the companies from app.yml

---
+++ job-finder/src/main/java/com/distributed/job_finder/config/SmartRecruitersConfig.java
@@ -0,0 +1,29 @@
+package com.distributed.job_finder.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import java.util.List;
+
+@Configuration
+@ConfigurationProperties(prefix = "app.scraper.smartrecruiters")
+public class SmartRecruitersConfig {
+
+    private String baseUrl = "https://api.smartrecruiters.com/v1/companies";
+    private List<String> targetBoards;
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+
+    public void setBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    public List<String> getTargetBoards() {
+        return targetBoards;
+    }
+
+    public void setTargetBoards(List<String> targetBoards) {
+        this.targetBoards = targetBoards;
+    }
+}
