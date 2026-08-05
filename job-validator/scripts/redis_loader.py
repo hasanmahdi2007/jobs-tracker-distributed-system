a3bb57021fe73a90c2493e28ae7f76d9dc747123
Hasan <hasan.mahdi7227@gmail.com>
2026-08-05 04:34:50 +0300

    added the greenhouse companies to the db and the app.yml and loaded redis

--- job-validator/scripts/redis_loader.py
+++ job-validator/scripts/redis_loader.py
@@ -6,8 +6,8 @@ import re
 # Connect to local Redis instance
 r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 
-# Target Redis queue key (change to queue:slugs:greenhouse when testing Greenhouse)
-REDIS_QUEUE_KEY = "queue:slugs:greenhouse"
+# Target Redis queue key mapped to the SmartRecruitersThrottledRunner
+REDIS_QUEUE_KEY = "queue:slugs:smartrecruiters"
 
 # Your target zip file path
 ZIP_PATH = r"C:\Users\user\Downloads\companies-2023-q4-sm.csv.zip"
