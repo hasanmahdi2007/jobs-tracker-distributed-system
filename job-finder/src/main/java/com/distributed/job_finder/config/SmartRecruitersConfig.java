c763df2f606b9f3e64ff7d209bc9b9325b4783b0
hasanmahdi2007 <hasan.mahdi7227@gmail.com>
2026-08-19 20:43:40 +0300

    boiler plate code

--- job-finder/src/main/java/com/distributed/job_finder/config/SmartRecruitersConfig.java
+++ job-finder/src/main/java/com/distributed/job_finder/config/SmartRecruitersConfig.java
@@ -3,7 +3,6 @@ package com.distributed.job_finder.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
